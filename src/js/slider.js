dc426dac57727dc2a949c7db105e695c64d49d23
HavryshDmytro <ckot444@gmail.com>
2022-05-10 22:32:09 +0300

    1

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,37 +1,57 @@
-const 
-    prev = document.getElementById('btn-prev'),
-    next = document.getElementById('btn-next'),
-    slides = document.querySelectorAll('.slide'),
-    dots = document.querySelectorAll('.dot');
+const prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
+      slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot');
       
 let index = 0;
 
 const activeSlide = n => {
-    for (slide of slides) {
-        slide.classlist.remove('active');
+    for(slide of slides) {
+        slide.classList.remove('active');
     }
     slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+    for(dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => { 
+    activeSlide(ind);
+    activeDot(ind);
 }
 
 const nextSlide = () => {
     if (index == slides.length - 1) {
         index = 0;
-        activeSlide(index);
+        prepareCurrentSlide(index);
     } else {
         index++;
-        activeSlide(index);
+        prepareCurrentSlide(index);
     }
 }
 
 const prevSlide = () => { 
     if (index == 0) { 
         index = slides.length - 1;
-        activeSlide(index);
+        prepareCurrentSlide(index);
     } else {
         index--;
-        activeSlide(index);
+        prepareCurrentSlide(index);
     }
 }
 
+dots.forEach((item, indexDot) => { 
+    item.addEventListener('click', () => { 
+        index = indexDot;
+        prepareCurrentSlide(index);
+    })
+})
+
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
+
+// setInterval(nextSlide, 3000);
