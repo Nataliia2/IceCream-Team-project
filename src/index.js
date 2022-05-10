eada9c8293b915e9e8f92889360fd20e23627f25
HavryshDmytro <ckot444@gmail.com>
2022-05-10 19:57:26 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
-import './js/mobile-menu';
-import './js/modal-form';
+// import './js/mobile-menu';
+// import './js/modal-form';
+import './js/slider';
---
+++ src/js/slider.js
@@ -0,0 +1,37 @@
+const 
+    prev = document.getElementById('btn-prev'),
+    next = document.getElementById('btn-next'),
+    slides = document.querySelectorAll('.slide'),
+    dots = document.querySelectorAll('.dot');
+      
+let index = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classlist.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+}
+
+const prevSlide = () => { 
+    if (index == 0) { 
+        index = slides.length - 1;
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
