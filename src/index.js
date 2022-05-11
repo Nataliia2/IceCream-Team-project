4209e5f5bf2dd2e97ce3cabd538941ab457fd15d
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-05-11 17:18:24 +0300

    bugfix slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './sass/main.scss';
-
+import './js/slider';
 import './js/contacts-modal';
 import './js/mobile-menu';
 import './js/modal-form';
 import './js/open-close';
 import './js/scroll-up';
-import './js/slider';
