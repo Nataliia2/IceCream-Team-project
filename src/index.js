eb43cd8a264a0758beeb32bc7c2ce8b9806d9ddb
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-05-11 21:04:57 +0300

    add js and styles in modal window

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import './js/slider';
 import './js/contacts-modal';
+import './js/about-modal';
 import './js/mobile-menu';
 import './js/modal-form';
 import './js/open-close';
