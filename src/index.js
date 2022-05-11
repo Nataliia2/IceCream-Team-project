57b75ffad416ffe691a188941aaa3b38cc0ef3c6
OspanovaVita <vitaospanova@ukr.net>
2022-05-11 11:14:26 +0300

    исправляет ошибки

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './sass/main.scss';
 import './js/mobile-menu';
 import './js/modal-form';
 import './js/scroll-up';
+import './js/buy-now';
--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -1,36 +1,15 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
 (() => {
-        const refs = {
-            openModalBtn: document.querySelector("[data-open-form]"),
-            closeModalBtn: document.querySelector("[data-close-form]"),
-            backdrop: document.querySelector("[data-backdrop-form]"),
-        };
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-        refs.openModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-        refs.backdrop.addEventListener("click", logBackdropClick);
-
-        function toggleModal() {
-            document.body.classList.toggle("modal-open");
-            refs.backdrop.classList.toggle("is-hidden");
-        }
-
-        function logBackdropClick() {
-            console.log("Это клик в бекдроп");
-        }
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
 })();
+
