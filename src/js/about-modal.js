88125b6f01881f174b315fb872506c1820e3c080
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-05-12 09:18:15 +0300

    about-modal window complet

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -3,12 +3,17 @@
     openModalBtn: document.querySelector("[modal-open]"),
     closeModalBtn: document.querySelector("[modal-close]"),
     modal: document.querySelector("[modal]"),
+    close: document.querySelector("[close]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.close.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+      refs.modal.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("is-closed");
+      
+    }
 })();
+
