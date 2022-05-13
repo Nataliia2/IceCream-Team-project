b07a54552148b4c599d7682bbc014a3bb95bce89
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-05-13 11:23:22 +0300

    bugfix modal products

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -19,13 +19,19 @@
 const refs = {
   openModalBtn: document.querySelector("[data-open-form]"),
   openModalBtn2: document.querySelector("[data-open-form2]"),
+  openModalBtn3: document.querySelector("[data-open-form3]"),
+  openModalBtn4: document.querySelector("[data-open-form4]"),
+  openModalBtn5: document.querySelector("[data-open-form5]"),
   closeModalBtn: document.querySelector("[data-close-form]"),
   backdrop: document.querySelector("[data-backdrop-form]"),
 };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.openModalBtn2.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
+  refs.openModalBtn4.addEventListener("click", toggleModal);
+  refs.openModalBtn5.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
 refs.backdrop.addEventListener("click", logBackdropClick);
 
